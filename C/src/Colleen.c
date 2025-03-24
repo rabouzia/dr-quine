ca39d3f2f47376f38131a2a5661e31565d84592f
Ramzy Zoldik <rabouzia@student.42.fr>
2025-03-24 04:56:34 +0100

    Colleen finished

--- C/src/Colleen.c
+++ C/src/Colleen.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-void called_fct()
-{
-	
-}
+void called_fct(){}
+
+char *str = "#include <stdio.h>%c%cvoid called_fct(){}%c%cchar *str = %c%s%c;%c%cint main ()%c{%c%cprintf(str,10,10,10,10,34,str,34,10,10,10,10,9,10,9,10,9,10,10,10,10,10);%c%c//comment 1%c%ccalled_fct();%c}%c%c// comment 2%c";
 
 int main ()
 {
-	printf("test\n");
+	printf(str,10,10,10,10,34,str,34,10,10,10,10,9,10,9,10,9,10,10,10,10,10);
 	//comment 1
 	called_fct();
 }
 
-//comment 2
+// comment 2
