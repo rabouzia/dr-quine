59a4fb39d6663c025944e28f3ee3b34a45e7ce74
Ramzy Zoldik <rabouzia@student.42.fr>
2025-04-02 13:28:29 +0200

    .

---
+++ Rust/target/debug/deps/Rust-538dc880879e283f.d
@@ -0,0 +1,5 @@
+/Users/ramzerk/.0_dev/42_Virology/dr-quine/Rust/target/debug/deps/libRust-538dc880879e283f.rmeta: src/main.rs
+
+/Users/ramzerk/.0_dev/42_Virology/dr-quine/Rust/target/debug/deps/Rust-538dc880879e283f.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/target/debug/deps/Rust-ea13fa9194772a3f.d
@@ -0,0 +1,5 @@
+/Users/ramzerk/.0_dev/42_Virology/dr-quine/Rust/target/debug/deps/libRust-ea13fa9194772a3f.rmeta: src/main.rs
+
+/Users/ramzerk/.0_dev/42_Virology/dr-quine/Rust/target/debug/deps/Rust-ea13fa9194772a3f.d: src/main.rs
+
+src/main.rs:
