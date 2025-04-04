badec9553d0b95b5fb7a95b2c0fc896a9446c925
Ramzy Zoldik <rabouzia@student.42.fr>
2025-04-04 13:25:14 +0200

    avancemenr

--- Rust/src/Sully.rs
+++ Rust/src/Sully.rs
@@ -2,12 +2,12 @@ use std::fs::File;
 use std::io::Write;
 use std::process::Command;
 
-const STR: &[u8] = b"use std::fs::File;\nuse std::io::Write;\nuse std::process::Command;\n\nconst STR: &[u8] = b\"edwed\";\nfn main (){\n\tlet i:i32   = 5; i--;\n\tlet file = File::create(format!(\"Sully_{}.rs\", i));\n\n\tlet _ = file.expect(\"REASON\").write(STR);\n\tlet mut _cmd = Command::new(\"rustc\").arg(\"src/Colleen.rs\").status();\n\tlet mut _cm = Command::new(\"./Colleen\").status();\n}";
+const STR: &[u8] = b"use std::fs::File;\nuse std::io::Write;\nuse std::process::Command;\n\nconst STR: &[u8] = b\"edwed\";\nfn main (){\n\tlet mut i:i32   = 5;\n\ti = i -1;\n\tlet file = File::create(format!(\"Sully_{}.rs\", i));\n\n\tlet _ = file.expect(\"REASON\").write(STR);\n\tlet mut _cmd = Command::new(\"rustc\").arg(\"src/Colleen.rs\").status();\n\tlet mut _cm = Command::new(\"./Colleen\").status();\n}";
 fn main (){
     let i:i32   = 5;
     let file = File::create(format!("Sully_{}.rs", i));
     let _ = file.expect("REASON").write(STR);
 
-    let mut _cmd = Command::new("rustc").arg("src/Colleen.rs").status();
-    let mut _cm = Command::new("./Colleen").status();
+    let mut _cmd = Command::new("rustc").arg(format!("Sully_{}.rs", i)).status();
+    let mut _cm = Command::new(format!("./Sully_{}", i)).status();
 }   
