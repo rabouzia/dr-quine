ea7d2b79fcdf7f5dfb034dd926fed1bc40763266
Ramzy Zoldik <rabouzia@student.42.fr>
2025-04-07 01:32:20 +0200

    fixed

--- Rust/target/debug/deps/sully-59903daf769f3afb.d
+++
@@ -1,5 +0,0 @@
-/Users/ramzerk/.0_dev/42_Virology/dr-quine/Rust/target/debug/deps/sully-59903daf769f3afb: src/Sully.rs
-
-/Users/ramzerk/.0_dev/42_Virology/dr-quine/Rust/target/debug/deps/sully-59903daf769f3afb.d: src/Sully.rs
-
-src/Sully.rs:
