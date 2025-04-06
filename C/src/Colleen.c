2b1ff7fc0be666b7d58684f9a824e1b3e3c7a006
Ramzy Zoldik <rabouzia@student.42.fr>
2025-04-07 01:46:09 +0200

    colleen c fixed

--- C/src/Colleen.c
+++ C/src/Colleen.c
@@ -2,13 +2,16 @@
 
 void called_fct(){}
 
-char *str = "#include <stdio.h>%c%cvoid called_fct(){}%c%cchar *str = %c%s%c;%c%cint main ()%c{%c%cprintf(str,10,10,10,10,34,str,34,10,10,10,10,9,10,9,10,9,10,10,10,10,10);%c%c//comment 1%c%ccalled_fct();%c}%c%c// comment 2%c";
+char *str = "#include <stdio.h>%1$c%1$cvoid called_fct(){}%1$c%1$cchar *str = %3$c%4$s%3$c;%1$c%1$cint main ()%1$c{%1$c%2$cprintf(str,10,9,34,str);%1$c%2$c/*%1$c%2$c%2$ccomment in%1$c%2$c*/%1$c%2$ccalled_fct();%1$c}%1$c/*%1$c%2$ccomment out%1$c*/";
 
 int main ()
 {
-	printf(str,10,10,10,10,34,str,34,10,10,10,10,9,10,9,10,9,10,10,10,10,10);
-	//comment 1
+	printf(str,10,9,34,str);
+	/*
+		comment in
+	*/
 	called_fct();
 }
-
-// comment 2
+/*
+	comment out
+*/
