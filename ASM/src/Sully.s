c31049b24636445ae10937e4d7a5c89af2410b7c
rabouzia <54402804+rabouzia@users.noreply.github.com>
2025-04-08 00:13:38 +0200

    almost finished

--- ASM/src/Sully.s
+++ ASM/src/Sully.s
@@ -1,44 +1,31 @@
 BITS 64
 section .data
-cmd db "ls -la", 0
-file db "Sully_%d.s", 5, 0
+cmd db "nasm -f elf64 Sully_4.s -o Sully_4.o && clang  -o Sully_4 Sully_4.o", 0
+file db "Sully_4.s", 0
 open_mode db "w", 0
-m db "%s, %2$c%3$c%4$c", 0
+suturingu db "%s,$c%3$c%4$c", 0
 section .text
 global main
 extern fprintf
 extern fopen
 extern system
 
-%macro ME 0
-	main:
-		push rbp
-		mov rbp, rsp
-        call file_create
-        call file_write
-        call file_exec
-		mov	rsp, rbp
-		pop rbp
-		ret
-    file_create:
-        lea rdi, [rel file]
-		lea rsi, [rel open_mode]
-		call fopen
-        ret
-    file_write:
-        mov rdi, rax
-		lea rsi, [rel m]
-		lea rdx, [rel m]
-		mov rcx, 10
-		mov r8d, 34
-		mov r9d, 9
-		call fprintf
-        ret
-    file_exec:
-        lea rdi, [rel cmd]
-        call system
-        ret
 
-
-%endmacro
-ME
+main:
+	push rbp
+	mov rbp, rsp
+	lea rdi, [rel file]
+	lea rsi, [rel open_mode]
+	call fopen
+	mov rdi, rax
+	lea rsi, [rel suturingu]
+	lea rdx, [rel suturingu]
+	mov rcx, 10
+	mov r8d, 34
+	mov r9d, 9
+	call fprintf
+	lea rdi, [rel cmd]
+	call system
+	mov	rsp, rbp
+	pop rbp
+	ret
--- Rust/target/debug/sully.d
+++
@@ -1,1 +0,0 @@
-/home/rabouzia/Documents/dr-quine/Rust/target/debug/sully: /home/rabouzia/Documents/dr-quine/Rust/src/Sully.rs
