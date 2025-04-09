f8bf006fd3ae4fabaaf0df80ff0a8e73c5710791
Ramzy Zoldik <rabouzia@student.42.fr>
2025-04-09 08:57:26 +0200

    forgot comment in asm but fixed

--- ASM/src/Colleen.s
+++ ASM/src/Colleen.s
@@ -1,6 +1,6 @@
 ;comment 1
 section .data
-msg db ";comment 1%1$csection .data%1$cmsg db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cglobal main%1$cextern printf%1$c%1$cmain:%1$c%4$clea rdi, [rel msg]%1$c%4$cmov esi, 10%1$c%4$cmov edx, 34%1$c%4$clea rcx, [rel msg]%1$c%4$cmov r8, 9%1$c%4$ccall ft_print%1$c%4$cret%1c%1$cft_print:%1$c%4$ccall printf%1$c%4$cret", 0
+msg db ";comment 1%1$csection .data%1$cmsg db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cglobal main%1$cextern printf%1$c%1$cmain:%1$c%4$clea rdi, [rel msg]%1$c%4$cmov esi, 10%1$c%4$c;comment 2%1$c%4$cmov edx, 34%1$c%4$clea rcx, [rel msg]%1$c%4$cmov r8, 9%1$c%4$ccall ft_print%1$c%4$cret%1c%1$cft_print:%1$c%4$ccall printf%1$c%4$cret", 0
 
 section .text
 global main
@@ -9,6 +9,7 @@ extern printf
 main:
 	lea rdi, [rel msg]
 	mov esi, 10
+	;comment 2
 	mov edx, 34
 	lea rcx, [rel msg]
 	mov r8, 9
--- ASM/src/Grace.s
+++ ASM/src/Grace.s
@@ -1,7 +1,8 @@
+;comment
 section .data
 file db "Grace_kid.s", 0
 open_mode db "w", 0
-m db "section .data%2$cfile db %3$cGrace_kid.s%3$c, 0%2$copen_mode db %3$cw%3$c, 0%2$cm db %3$c%s%3$c, 0%2$csection .text%2$cglobal main%2$cextern fprintf%2$cextern fopen%2$cextern fclose%2$c%2$c%%macro ME 0%2$c%4$cmain:%2$c%4$c%4$cpush rbp%2$c%4$c%4$cmov rbp, rsp%2$c%2$c%4$c%4$clea rdi, [rel file]%2$c%4$c%4$clea rsi, [rel open_mode]%2$c%4$c%4$ccall fopen%2$c%2$c%4$c%4$cmov r15, rax%2$c%4$c%4$cmov rdi, rax%2$c%4$c%4$clea rsi, [rel m]%2$c%4$c%4$clea rdx, [rel m]%2$c%4$c%4$cmov rcx, 10%2$c%4$c%4$cmov r8d, 34%2$c%4$c%4$cmov r9d, 9%2$c%4$c%4$ccall fprintf%2$c%4$c%4$cmov rdi, r15%2$c%4$c%4$ccall fclose%2$c%2$c%4$c%4$cmov	rsp, rbp%2$c%4$c%4$cpop rbp%2$c%4$c%4$cret%2$c%%endmacro%2$cME", 0
+m db ";comment%2$csection .data%2$cfile db %3$cGrace_kid.s%3$c, 0%2$copen_mode db %3$cw%3$c, 0%2$cm db %3$c%s%3$c, 0%2$csection .text%2$cglobal main%2$cextern fprintf%2$cextern fopen%2$cextern fclose%2$c%2$c%%macro ME 0%2$c%4$cmain:%2$c%4$c%4$cpush rbp%2$c%4$c%4$cmov rbp, rsp%2$c%2$c%4$c%4$clea rdi, [rel file]%2$c%4$c%4$clea rsi, [rel open_mode]%2$c%4$c%4$ccall fopen%2$c%2$c%4$c%4$cmov r15, rax%2$c%4$c%4$cmov rdi, rax%2$c%4$c%4$clea rsi, [rel m]%2$c%4$c%4$clea rdx, [rel m]%2$c%4$c%4$cmov rcx, 10%2$c%4$c%4$cmov r8d, 34%2$c%4$c%4$cmov r9d, 9%2$c%4$c%4$ccall fprintf%2$c%4$c%4$cmov rdi, r15%2$c%4$c%4$ccall fclose%2$c%2$c%4$c%4$cmov	rsp, rbp%2$c%4$c%4$cpop rbp%2$c%4$c%4$cret%2$c%%endmacro%2$cME", 0
 section .text
 global main
 extern fprintf
