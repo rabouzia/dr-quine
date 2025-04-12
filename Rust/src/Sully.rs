82bb6a4ceb2652a49f745485b46ea36607cfb49a
rabouzia <54402804+rabouzia@users.noreply.github.com>
2025-04-12 19:55:35 +0200

    rust fixed

--- Rust/src/Sully.rs
+++ Rust/src/Sully.rs
@@ -2,12 +2,14 @@
 use std::fs::File;
 use std::io::Write;
 use std::process::Command;
+use std::fs;
 
 fn main() {
 	let mut i:i32 = 5;
 	if i <= 0 { return; }
-	i -= 1;
-	macro_rules! script {() => {"// comment 1\nuse std::fs::File;\nuse std::io::Write;\nuse std::process::Command;\n\nfn main() {\n\tlet mut i:i32 = %d;\n\tif i == 0 { return; }\n\ti -= 1;\n\tmacro_rules! script {() => {{:?}}}\n\tlet mut file = File::create(format!(\"Sully_{}.rs\", i)).unwrap();\n\tlet code = script!().replacen(\"%d\", &(i).to_string(), 1);\n\tlet _ = write!(file, \"{}\", code.replacen(\"{:?}\", &format!(\"{:?}\", script!()), 1));\n\tlet mut _cmd = Command::new(\"rustc\").arg(format!(\"Sully_{}.rs\", i)).status();\n\tlet mut _cm = Command::new(format!(\"./Sully_{}\", i)).status();\n}\n"}}
+	if fs::exists("Sully_5.rs").expect("REASON")
+		{i -= 1;}
+	macro_rules! script {() => {"// comment 1\nuse std::fs::File;\nuse std::io::Write;\nuse std::process::Command;\nuse std::fs;\n\nfn main() {\n\tlet mut i:i32 = %d;\n\tif i <= 0 { return; }\n\tif fs::exists(\"Sully_5.rs\").expect(\"REASON\")\n\t\t{i -= 1;}\n\tmacro_rules! script {() => {{:?}}}\n\tlet mut file = File::create(format!(\"Sully_{}.rs\", i)).unwrap();\n\tlet code = script!().replacen(\"%d\", &(i).to_string(), 1);\n\tlet _ = write!(file, \"{}\", code.replacen(\"{:?}\", &format!(\"{:?}\", script!()), 1));\n\tlet mut _cmd = Command::new(\"rustc\").arg(format!(\"Sully_{}.rs\", i)).status();\n\tlet mut _cm = Command::new(format!(\"./Sully_{}\", i)).status();\n}\n"}}
 	let mut file = File::create(format!("Sully_{}.rs", i)).unwrap();
 	let code = script!().replacen("%d", &(i).to_string(), 1);
 	let _ = write!(file, "{}", code.replacen("{:?}", &format!("{:?}", script!()), 1));
